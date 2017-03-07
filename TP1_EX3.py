df2c642805392cb39254cb23c62f93e049da290f
Distant Thunder <temptempor@gmail.com>
2017-03-07 11:05:06 +0100

    TP1 EX3 version intention

--- TP1_EX3.py
+++ TP1_EX3.py
@@ -1,7 +1,11 @@
-# Déclaration d'une liste en "intention" (sans variables intermédiaires)
+# Déclaration d'une liste en "extension" 
 
 my_list = []
 for i in "abc":
     for j in "de":
         my_list.append(i+j)
 print(my_list)
+
+# Version en "intention" de la même liste
+ma_liste=[i+j for i in "abc" for j in "de"]
+print(ma_liste)
