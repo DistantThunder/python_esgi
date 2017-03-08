717f4124b01d07d5ed2f2698fc48fe863fdba1a9
Distant Thunder <temptempor@gmail.com>
2017-03-08 12:32:16 +0100

    copie fichiers

---
+++ TP1_EX_copie_fichiers.py
@@ -0,0 +1,3 @@
+def filter(file):
+    src_file = open('file','r')
+    filtered_file = 
--- projet_ldap/mock_implementation.py
+++ projet_ldap/mock_implementation.py
@@ -2,6 +2,9 @@ from random import choice
 from hashlib import md5
 import string
 
+# Base de données utilisateurs stockée dans un dictionnaire.
+
+userdb = {}
 
 char_space=string.ascii_letters+string.digits
 print("Caractères utilisés : ", char_space,"\n")
@@ -11,16 +14,41 @@ print("Caractères utilisés : ", char_space,"\n")
 def genpasswd():
     password = ""
 # On va boucler sur le char_space pour obtenir un peu plus de randomness
+# par multiple invocations de "choice".
     for i in range(8):
         password += choice(char_space)
     return password
 
 
+# Fonction de hashage : prend en argument un mot de passe et génère une empreinte avec algo MD5
 def hashing(input_password):
         md5hash = md5()
         md5hash.update(input_password.encode(encoding='ascii', errors='strict'))
         return md5hash.hexdigest()
 
-motpasse = genpasswd()
-print("Mot de passe généré: ' ", motpasse, " '\n")
-print("Hash MD5 du mot de passe: ' ", hashing(motpasse), " '\n")
+
+def add_user():
+    username = input("Saisissez un nouvel utilisateur:\n>>> ")
+    motpasse = genpasswd()
+    userdb[username]=hashing(motpasse)
+    print("Utilisateur => ' {:s} '\nMot de passe => {:s} \nHash => {:s}".format(username, motpasse, userdb[username]))
+    return 0
+
+
+## Fonction "check_user" prend un nom d'utilisateur en argument et cherche dans le dictionnaire.
+## Sinon demande une saisie à l'utilisateur
+def check_user(username=""):
+    # Un string vide renverra 'False'
+    if username:
+        username = input("Which user are you looking for?\n>>> ")
+
+    print("Checking user {:s}".format(username))
+    return userdb.__contains__(username)
+
+
+#def delete_user():
+
+
+add_user()
+print(check_user("yann"))
+#delete_user()
