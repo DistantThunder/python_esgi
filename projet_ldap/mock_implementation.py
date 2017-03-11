21d86764e3720019aaf4c22f0fa81d235ea1862f
Distant Thunder <temptempor@gmail.com>
2017-03-11 19:46:36 +0100

    delete_user v.0.1

--- projet_ldap/mock_implementation.py
+++ projet_ldap/mock_implementation.py
@@ -6,27 +6,26 @@ import sqlite3
 
 
 ##### VARIABLES
-# Base de données utilisateurs stockée dans un dictionnaire.
-userdb = {}
 # Arguments en ligne de commande
-script, argv = argv
+script = argv
 # Définition de l'espace de caractères pour nos mots de passe
-char_space=string.ascii_letters+string.digits
+char_space = string.ascii_letters+string.digits
 
 # Affichage dudit espace sur la sortie standard
 print("Caractères utilisés : ", char_space,"\n")
 
 # Connection à la base de données SQLITE3
+# Créée automatiquement si elle n'existe pas et initi
 db_object = sqlite3.connect("myldap.sqlite3")
-dbcon_object = db_object.cursor()
+dbcon_cursor = db_object.cursor()
 
 
-###### FONCTIONS
+#############################################################################>>>>>>>>>>>>>>>>> FONCTIONS
 # Initialise la base de données si besoin
 def db_initialize(tableName):
     # REQUÊTES SQLITE EFFECTUÉES AVEC SUBSTITUTION '?' AFIN DE MINIMISER RISQUES INJECTION SQL
-    dbcon_object.execute("SELECT 1 FROM sqlite_master WHERE type = ? AND name = ?", ["table", tableName])
-    qResult = dbcon_object.fetchone()
+    dbcon_cursor.execute("SELECT 1 FROM sqlite_master WHERE type = ? AND name = ?", ["table", tableName])
+    qResult = dbcon_cursor.fetchone()
     # L'EVALUATION RENVOIE 'False' SI qResult N'A AUCUN TYPE (==VIDE). DONC :
     # SI qResult *- A UN TYPE -* ET DONC *-N'EST PAS VIDE-*, LA REQUÊTE SQLite PRÉCÉDENTE
     # A RENVOYÉ UNE VALEUR -> LA TABLE A ÉTÉ TROUVÉE.
@@ -36,8 +35,8 @@ def db_initialize(tableName):
     else:
         print("Table ' {:s} ' does not exist! Creating it before continuing...\n".format(tableName))
         # NOTE : LES NOMS DE TABLES NE **PEUVENT PAS** ÊTRE PASSÉS EN SUBSTITUTION DE PARAMÈTRE '?'!!!!
-        dbcon_object.execute("DROP TABLE IF EXISTS %s" % tableName)
-        dbcon_object.execute("""CREATE TABLE %s (
+        dbcon_cursor.execute("DROP TABLE IF EXISTS %s" % tableName)
+        dbcon_cursor.execute("""CREATE TABLE %s (
                                 uid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                                 username text,
                                 password CHAR(32)
@@ -69,7 +68,7 @@ def add_user():
     new_user = (username,pwHash)
     print("Utilisateur => ' {:s} '\nMot de passe => {:s} \nHash => {:s}".format(new_user[0], motpasse, new_user[1]))
     print("New user is : ", new_user)
-    dbcon_object.execute("INSERT INTO users VALUES(null,?,?)", new_user)
+    dbcon_cursor.execute("INSERT INTO users VALUES(null,?,?)", new_user)
     return 0
 
 
@@ -81,8 +80,8 @@ def check_user(user=""):
         user = input("Which user are you looking for?\n>>> ")
 
     print("Checking user ' {:s} ' ...".format(user))
-    dbcon_object.execute("SELECT uid,username FROM users WHERE username = ?", [user])
-    return dbcon_object.fetchall()
+    dbcon_cursor.execute("SELECT uid,username FROM users WHERE username = ?", [user])
+    return dbcon_cursor.fetchall()
 
 
 def delete_user():
@@ -92,17 +91,16 @@ def delete_user():
             print("Un utilisateur a été trouvé.")
             print("Voulez-vous effacer l'utilisateur {:s} ? o/N".format(qResult[0][1]))
             answer = input(">>>")
-            if answer == ("O" or "o"):
+            if answer == "O" or "o":
                 print("ATTENTION! Cette opération est irréversible!!\nEntrez 'OK' en toutes lettres pour confirmer:\n")
                 confirm = input(">>>")
                 if confirm == "OK":
                     print("Deleting {:s}...\n".format(qResult[0][1]))
-                    dbcon_object.execute("DELETE FROM users WHERE uid = ? AND username = ?", [qResult[0][0], qResult[0][1]])
-                    print("Done!")
+                    dbcon_cursor.execute("DELETE FROM users WHERE uid = ? AND username = ?", [qResult[0][0], qResult[0][1]])
                 else:
                     print("Confirmation non reçue. Arrêt...\n")
                     exit(1)
-            elif answer == ("N" or "n"):
+            elif answer == "N" or "n":
                 print("Il s'agit du seul utilisateur trouvé. Arrêt...\n")
                 exit(0)
             else:
@@ -127,7 +125,7 @@ def delete_user():
 
 
 
-###### EXÉCUTION
+#####################################################>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXÉCUTION
 
 admin = 1
 
@@ -135,7 +133,7 @@ db_initialize("users")
 
 ### SEUL L'ADMIN PEUT SUPPRIMER DES UTILISATEURS ARBITRAIREMENT
 if admin == 1:
-    delete_user()
+
 else:
     print("Your are not admin!!\nExiting...")
     exit(1)
@@ -146,5 +144,5 @@ db_object.commit()
 # Fermeture de la connexion à la BDD
 db_object.close()
 
-del dbcon_object
+del dbcon_cursor
 del db_object
